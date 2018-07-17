429616a7e42f84385d80fd439a13a319b2714f64
apand3 <abhi.pandya91@outlook.com>
2018-07-17 04:09:06 -0500

    pack

--- server.js
+++ server.js
@@ -23,11 +23,13 @@ app.use('/api/profile',profile);
 app.use('/api/posts',posts);
 
 //production
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(`client/build`));
-    app.get("*",(req,res)=>{
-        res.sendfile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 const port=process.env.PORT || 5000;
 app.listen(port,()=>console.log(`Server running on port ${port}`));
